3cc2fa903ec45ab3a5646f1d376c12eda013dccb
Sebastian Contreras <sebasfac2017@gmail.com>
2023-05-25 13:10:36 -0300

    Como se realiza un fetch de datos

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function IndexPage() {
-  return (
-    <h1>Hello World</h1>
-  )
+async function fetchUser() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
 }
 
-export default IndexPage
+async function IndexPage() {
+  const users = await fetchUser();
+
+  return (
+    <>
+      <h1>Usuarios</h1>
+      <p>{JSON.stringify(users)}</p>
+    </>
+  );
+}
+
+export default IndexPage;
