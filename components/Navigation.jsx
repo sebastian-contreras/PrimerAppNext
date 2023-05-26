1c53f9f2063d7682cc2d159ec78b42de70b98b6a
Sebastian Contreras <sebasfac2017@gmail.com>
2023-05-25 22:48:17 -0300

    Falta estilizar

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,23 +1,31 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Navigation() {
   return (
-    <>
-    <h2>Navigation</h2>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/services">Services</Link>
-          </li>
-        </ul>
-    </>
-  )
+    <nav className="navbar navbar-dark bg-primary navbar-expand-lg ">
+  <div className="container-fluid">
+    <Link className=" navbar-brand fw-bold" href="/">Perfiles</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse text-end" id="navbarNavDropdown">
+      <ul className="ms-auto me-5 navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" href="/services">Servicios</Link>
+        </li>
+        <li className="nav-item">
+        <Link className="nav-link active" aria-current="page" href="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
--- components/Users.jsx
+++ components/Users.jsx
@@ -4,23 +4,39 @@ import React from "react";
 
 function Users({ users }) {
   const router = useRouter();
+    // <ul>
+    //     <li
+    //       key={user.id}
+    //       onClick={() => {
+    //         router.push(`/users/${user.id}`);
+    //       }}
+    //     >
+    //       <h5>
+    //         {user.id} {user.first_name} {user.last_name}
+    //       </h5>
+    //       <p>{user.email}</p>
+    //       <img src={user.avatar} alt={user.email} />
+    //     </li>
+    // </ul>
   return (
-    <ul>
+    <>  
       {users.map((user) => (
-        <li
-          key={user.id}
-          onClick={() => {
-            router.push(`/users/${user.id}`);
-          }}
-        >
-          <h5>
-            {user.id} {user.first_name} {user.last_name}
-          </h5>
-          <p>{user.email}</p>
-          <img src={user.avatar} alt={user.email} />
-        </li>
-      ))}
-    </ul>
+    <div onClick={()=>{router.push(`/users/${user.id}`);}} className="card mb-3" style={{maxWidth:'540px'}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src={user.avatar} className="img-fluid rounded-start" alt={user.email} />
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">{user.id} {user.first_name} {user.last_name}</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+    ))}
+    </>
   );
 }
 
